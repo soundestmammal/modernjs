1f9bef8e0339c061e72e86beed706f0e70a0d91b
Robert Checco <robert.checco@gmail.com>
2019-06-04 12:52:08 -0400

    Messy approach to create an admin user...

--- 12_DefaultFunctionArgs/user.js
+++ 12_DefaultFunctionArgs/user.js
@@ -5,3 +5,13 @@ function User(id) {
 function generateId() {
     return Math.random() * 999999;
 }
+
+function createAdminUser() {
+    user.admin = true;
+
+    return user;
+}
+
+// To make this work...
+// createAdminUser(new User(generateId()));
+// Wow that is messy
