fc2f458bce32cfae64cfeead19d2fecfc4553b92
Robert Checco <robert.checco@gmail.com>
2019-06-04 16:11:42 -0400

    Math Library Example Pre-Refactor

--- 13_RestSpreadOperators/app.js
+++ 13_RestSpreadOperators/app.js
@@ -57,3 +57,10 @@ function validateShoppingList(...items) {
 }
 
 console.log(validateShoppingList('oranges', 'bread', 'eggs'));
+
+const MathLibrary = {
+    calculateProduct(a,b) {
+        return a * b;
+    }
+};
+
